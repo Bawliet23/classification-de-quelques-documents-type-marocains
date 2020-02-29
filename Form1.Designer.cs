c320dd201573566ffdf1681c62c884243dcf71fc
Bawliet23 <mohamedbhajy23@gmail.com>
2020-02-29 14:18:29 +0100

    Initial commit
    Classification of moroccan documents using the OCR and template.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace pfe3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(96, 27);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(394, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 306);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(410, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using AForge;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using System.IO;
+using Tesseract;
+
+namespace pfe3
+{
+    
+    public partial class Form1 : Form
+    {
+        string fname1;
+        Bitmap img1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            label1.Text = "";
+            openFileDialog1.FileName = "";
+            openFileDialog1.Title = "Images";
+            openFileDialog1.Filter = "All Images|*.jpg; *.bmp; *.png";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName.ToString() != "")
+            {
+                fname1 = openFileDialog1.FileName.ToString();
+                img1 = new Bitmap(fname1);
+                pictureBox1.Visible = true;
+                pictureBox1.Image = img1;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double width = Math.Floor(img1.Width / img1.HorizontalResolution);
+            double height = Math.Floor(img1.Height / img1.VerticalResolution);
+            label1.Text = img1.HorizontalResolution + " " + img1.VerticalResolution;
+            img1 = Grayscale.CommonAlgorithms.BT709.Apply(img1);
+            Threshold filter = new Threshold(150);
+            filter.ApplyInPlace(img1);
+            DocumentSkewChecker skewChecker = new DocumentSkewChecker();
+            double angle = skewChecker.GetSkewAngle(img1);
+            RotateBilinear rotationFilter = new RotateBilinear(-angle);
+            rotationFilter.FillColor = Color.White;
+            img1 = rotationFilter.Apply(img1);
+            pictureBox1.Image = img1;
+            TesseractEngine engine = new TesseractEngine(@"C:\Users\moham\source\repos\pfe3\tessdata\", "fra",EngineMode.Default);
+            if (((width == 3) && (height == 2)) || (width == 2 && height == 3))
+            {
+                if((width == 2 && height == 3))
+                {
+                    rotationFilter = new RotateBilinear(-90);
+                    rotationFilter.FillColor = Color.White;
+                    img1 = rotationFilter.Apply(img1);
+                    Console.WriteLine("if1");
+                    
+                }
+                string ocrText = engine.Process(img1, new Rect(700, 450, 150, 100)).GetText();
+                engine.Dispose();
+                ocrText = ocrText.Replace(" "," ");
+                Console.WriteLine(ocrText+" "+ocrText.Length);
+                if (ocrText.Length <= 9)
+                label1.Text = "carte national " + ocrText;
+                else
+                {
+                    rotationFilter = new RotateBilinear(180);
+                    rotationFilter.FillColor = Color.White;
+                    img1 = rotationFilter.Apply(img1);
+                    engine = new TesseractEngine(@"C:\Users\moham\source\repos\pfe3\tessdata\","fra", EngineMode.Default);
+                    ocrText = engine.Process(img1, new Rect(700, 450, 150, 100)).GetText();
+                    engine.Dispose();
+                    ocrText = ocrText.Replace(" ", "");
+                }
+                label1.Text = "carte national " + ocrText;
+                pictureBox1.Image = img1;
+                engine.Dispose();
+            }
+            else if ((width == 8 && height == 11) || (width == 11 && height == 8))
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+    }
+}
